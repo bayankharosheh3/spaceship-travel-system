ce67cbb81e93a06f17c38d5e2d6bb5c33c416ab8
bayankharosheh3 <bayankharoseh3@gmail.com>
2024-06-14 13:13:17 +0300

    database connection using Sequelize with MySQL.

--- api/app.js
+++ api/app.js
@@ -5,6 +5,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
 });
